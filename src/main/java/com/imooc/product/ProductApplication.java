e36eceda8e3b196b8718f6e116e64a74d222c9b0
huangxinhe3 <huangxinhe3@jd.com>
2020-04-26 21:55:06 +0800

    product

---
+++ src/main/java/com/imooc/product/ProductApplication.java
@@ -0,0 +1,17 @@
+package com.imooc.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+//@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+public class ProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/imooc/product/controller/ProductController.java
@@ -0,0 +1,21 @@
+package com.imooc.product.controller;
+
+/**
+ * @ClassName ProductController
+ * @Description TODO
+ * @Author hxh
+ * @Date 2020/4/26 9:42 下午
+ * @Version 1.0
+ */
+public class ProductController {
+
+    /**
+     * 1.查询所有在架的商品
+     * 2.获取类目type列表
+     * 3.查询类目
+     * 4.构造数据
+     */
+    public void list(){
+
+    }
+}
