7391a52c41bc9cd5a99a84ec4de9d9a051865908
huangxinhe3 <huangxinhe3@jd.com>
2020-05-14 00:00:42 +0800

    order Feign远程调用product

--- src/test/java/com/imooc/product/service/ProductServiceTest.java
+++ src/test/java/com/imooc/product/service/ProductServiceTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Component
@@ -18,6 +19,12 @@ public class ProductServiceTest extends ProductApplicationTests {
     @Test
     public void testFindUpAll() throws Exception{
         List<ProductInfo> list = productService.findUpAll();
+        Assert.assertTrue(list.size()>0);
+    }
+
+    @Test
+    public void testFindList() throws Exception{
+        List<ProductInfo> list = productService.findList(Arrays.asList("157875196366160022","157875227953464068"));
         Assert.assertTrue(list.size()>0);
     }
 }
